c0963fdcf8c7acb3f48b5ff739c80231e09d5ffe
RetoranPetra <flyro@live.co.uk>
2023-04-06 16:46:17 +0100

    Updated colours and spoon.

---
+++ colours.h
@@ -0,0 +1,60 @@
+//Vim colors
+//Background and foreground
+static const char col_black       [] = "#011627";
+static const char col_white       [] = "#c3ccdc";
+//Variations of blue-grey
+static const char col_black_blue  [] = "#081e2f";
+static const char col_dark_blue   [] = "#092236";
+static const char col_deep_blue   [] = "#0e293f";
+static const char col_slate_blue  [] = "#2c3043";
+static const char col_pickle_blue [] = "#38507a";
+static const char col_regal_blue  [] = "#1d3b53";
+static const char col_steel_blue  [] = "#4b6479";
+static const char col_grey_blue   [] = "#7c8f8f";
+static const char col_cadet_blue  [] = "#a1aab8";
+static const char col_ash_blue    [] = "#acb4c2";
+static const char col_white_blue  [] = "#d6deeb";
+//Core theme colors
+static const char col_yellow      [] = "#e3d18a";
+static const char col_peach       [] = "#ffcb8b";
+static const char col_tan         [] = "#ecc48d";
+static const char col_orange      [] = "#f78c6c";
+static const char col_orchid      [] = "#e39aa6";
+static const char col_red         [] = "#fc514e";
+static const char col_watermelon  [] = "#ff5874";
+static const char col_violet      [] = "#c792ea";
+static const char col_purple      [] = "#ae81ff";
+static const char col_indigo      [] = "#5e97ec";
+static const char col_blue        [] = "#82aaff";
+static const char col_malibu      [] = "#87bcff";
+static const char col_turquoise   [] = "#7fdbca";
+static const char col_emerald     [] = "#21c7a8";
+static const char col_green       [] = "#a1cd5e";
+//Extra colors
+static const char col_cyan_blue   [] = "#296596";
+static const char col_bay_blue    [] = "#24567F";
+
+//Old Alacritty colours
+static const char col_bg [] = "#0D0F1C";
+static const char col_fg [] = "#86FEE6";
+static const char col_dim_fg [] = "#02DEB2";
+static const char col_br_fg [] = "#00FF00";
+static const char col_candy_blue [] = "#0075A2";
+static const char col_candy_pink [] = "#E40066";
+//Old terminal colours
+static const char col_trm_black[] =   "#222222";
+static const char col_trm_red[] =     "#E40066";
+static const char col_trm_green[] =   "#009900";
+static const char col_trm_yellow[] =  "#FB4D3D";
+static const char col_trm_blue[] =    "#0075A2";
+static const char col_trm_magenta[] = "#990D5B";
+static const char col_trm_cyan[] =    "#E0E000";
+static const char col_trm_white[] =   "#c5c8c6";
+//Old terminal bright colours
+static const char col_trm_br_black[] =   "#666666";
+static const char col_trm_br_red[] =     "#F60099";
+static const char col_trm_br_green[] =   "#00FF00";
+static const char col_trm_br_yellow[] =  "#FD6B5E";
+static const char col_trm_br_blue[] =    "#7aa6da";
+static const char col_trm_br_magenta[] = "#c397d8";
+static const char col_trm_br_cyan[] =    "#FFFF00";
--- config.h
+++ config.h
@@ -1,9 +1,10 @@
+#include "colours.h"
 /* See LICENSE file for copyright and license details. */
 
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -14,16 +15,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "iosevka:size=10" };
 static const char dmenufont[]       = "iosevka:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_gray1[]       = "#222222";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+	[SchemeNorm] = { col_white, col_black, col_black },
+	[SchemeSel]  = { col_white, col_slate_blue,  col_grey_blue },
+	[SchemeHid]  = { col_pickle_blue,  col_black, col_black  },
 };
 
 /* tagging */
@@ -66,18 +67,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",col_black, "-nf", col_white, "-sb", col_slate_blue, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_space,  togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      focusstackhid,  {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
+//Want a MODKEY version of this that scrolls between workspaces.
+	{ ALTKEY,                       XK_Tab,      focusstackvis,  {.i = +1 } },
+	{ ALTKEY|ShiftMask,             XK_Tab,      focusstackvis,  {.i = -1 } },
+//{ MODKEY|ShiftMask,             XK_j,      focusstackhid,  {.i = +1 } },
+//{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -85,6 +87,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+//Need to test swap out with own layouts thing that cycles through layouts.
   { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
   { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 //{ MODKEY,                       XK_f,      setlayout,      {0} },
